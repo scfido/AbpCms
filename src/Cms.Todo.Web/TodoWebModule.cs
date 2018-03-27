da6def1f77c4eeb04e5f06cbd107f0afc37ac86f
fuyun <scfido@gmail.com>
2018-03-27 09:14:22 +0800

    调整单元测试代码文件编码格式

--- src/Cms.Todo.Web/TodoWebModule.cs
+++ src/Cms.Todo.Web/TodoWebModule.cs
@@ -23,9 +23,8 @@ namespace Cms.Todo.Web
             var env = this.IocManager.Resolve<IHostingEnvironment>();
             if (!env.IsDevelopment())
             {
-                //使用源嵌入到dll中View和js、image、css等静态资。
-                //下面的代码生效的前提是在Visual Studio解决方案资源管理器中，把这些静态资源
-                //设置为嵌入的资源。
+                //使用嵌入到dll中View和js、image、css等静态资源。
+                //下面的代码生效的前提是在Visual Studio解决方案资源管理器中，把这些文件属性设置为嵌入的资源。
                 //在开发时不希望每次改动静态资源都要重新编译，所以这段代码只在发布产品的状态下运行。
                 //开发模式这些静态文件由Startup文件中单独指定了路径。
                 Configuration.EmbeddedResources.Sources.Add(
